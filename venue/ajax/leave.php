3f2c07262e59401d078650c81299ea7b68e553e6
Goose <goosemixes@gmail.com>
2014-09-03 12:02:16 -0400

    Relocating Repository
    Looks like a big change, little to nothing was actually changed

--- venue/ajax/leave.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo '<br><br><br><br><center><font size="400px">bye</font></center>';
-session_start();
-//logout
-    session_destroy();
-    echo '<META HTTP-EQUIV="Refresh" Content="0; URL=/index.html">';
-
-?>
